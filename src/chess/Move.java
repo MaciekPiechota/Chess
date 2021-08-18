033de4bfdf293042294c588b5f05ceca633ceaad
Maciej Piechota <mp431548@students.mimuw.edu.pl>
2021-08-18 02:39:09 +0200

    cleaning up code changing language to english in most of the files

---
+++ src/chess/Move.java
@@ -0,0 +1,13 @@
+package chess;
+
+import chess.pieces.Tile;
+
+public class Pair {
+    public Tile first;
+    public Tile second;
+
+    public Pair(Tile first, Tile second) {
+        this.first = first;
+        this.second = second;
+    }
+}
